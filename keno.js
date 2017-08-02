71bb9d2bb0aa2a2951effd32eafad8a44ff1f1e3
HotWhls55 <TimTaylor_HomeFolder@Timothys-MacBook-Pro.local>
2017-08-02 16:03:57 -0700

    changing something on master to fetch/pull into new_test_branch

--- keno.js
+++ keno.js
@@ -7,7 +7,7 @@
 //                  randomNumberPicker - .
 //
 // Changing Something August 2nd, 2017
-// Changing Something Else - testing new_test_branch
+// Removed somethin changed, to test fetching from master
 //***************************************************************
 
 var pickedNumbers = [];
